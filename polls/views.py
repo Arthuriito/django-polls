dcf3941c2ef95a8d8bb24b2e852448313cf4d3db
Arthuriito <arthurxavier1408@gmail.com>
2023-09-05 10:48:39 -0300

    atualização completa repositório *Tudo que faltava

--- polls/views.py
+++ polls/views.py
@@ -9,7 +9,8 @@ from django.http import HttpResponse
 # Create your views here
 
 def index(request):
-    return HttpResponse('Olá... seja bem vindo à enquete')
+    context = {'titulo': 'Página Principal'}
+    return render(request, 'home.html', context)
 
 def sobre(request):
     return HttpResponse('Olá este é um app de enquete')
@@ -26,4 +27,53 @@ def exibe_questao(request, question_id):
 def ultimas_perguntas(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/perguntas.html', context)
+    return render(request, 'polls/perguntas_recentes.html', context)
+
+def vote(request, question_id):
+    return HttpResponse(f"Você vai votar na pergunta ")
+
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+class QuestionCreateView(CreateView):
+    model = Question
+    template_name = 'polls/question_form.html'
+    fields = ('question_text', 'pub_date', )
+    success_url = reverse_lazy('polls_list')
+    def get_context_data(self, **kwargs):
+        context = super(QuestionCreateView, self).get_context_data(**kwargs)
+        context['form_title'] = 'Criando uma pergunta'
+        return context
+
+class QuestionUpdateView(UpdateView):
+    model = Question
+    template_name = 'polls/question_form.html'
+    fields = ('question_text', 'pub_date', )
+    success_url = reverse_lazy('polls_list')
+    def get_context_data(self, **kwargs):
+        context = super(QuestionUpdateView, self).get_context_data(**kwargs)
+        context['form_title'] = 'Editando a pergunta'
+        return context
+
+class QuestionDeleteView(DeleteView):
+    model = Question
+    template_name = 'polls/question_confirm_delete_form.html'
+    success_url = reverse_lazy('polls_list')
+
+from django.views.generic import DetailView, ListView, TemplateView
+
+class QuestionDetailView(DetailView):
+    model = Question
+    template_name = 'polls/question_detail.html'
+    context_object_name = 'question'
+
+class QuestionListView(ListView):
+    model = Question
+    template_name = 'polls/question_list.html'
+    context_object_name = 'questions'
+
+class SobreTemplateView(TemplateView):
+    template_name = 'polls/sobre.html'
+
+
+
