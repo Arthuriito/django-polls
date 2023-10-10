0122a5a3c806e4959bd37ba9dd245fb0e04d7d02
Arthuriito <arthurxavier1408@gmail.com>
2023-10-10 10:21:14 -0300

    atualização django... de novo

---
+++ polls/views.py
@@ -0,0 +1,104 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import DetailView, ListView
+from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from polls.models import Question, Choice
+from django.http import HttpResponse
+from django.views.generic import DetailView, ListView, TemplateView
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+# Create your views here
+
+def index(request):
+    context = {'titulo': 'Página Principal'}
+    return render(request, 'polls/home.html', context)
+
+@login_required
+
+def sobre(request):
+    return HttpResponse('Olá este é um app de enquete')
+
+def exibe_questao(request, question_id):
+    questao = Question.objects.get(id=question_id)
+    return HttpResponse(f'Questão 1: {questao.question_text}')
+
+    if questao is not None:
+        return HttpResponse(questao.question_text)
+    
+    return HttpResponse('Não existe questao a exibir')
+
+def ultimas_perguntas(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/perguntas_recentes.html', context)
+
+def vote(request, question_id):
+    return HttpResponse(f"Você vai votar na pergunta ")
+
+
+
+class QuestionCreateView(CreateView):
+    model = Question
+    template_name = 'polls/question_form.html'
+    fields = ('question_text', 'pub_date', )
+    success_url = reverse_lazy('polls_all')
+    success_message = 'Pergunta criada com sucesso.'
+    def get_context_data(self, **kwargs):
+        context = super(QuestionCreateView, self).get_context_data(**kwargs)
+        context['form_title'] = 'Criando uma pergunta'
+        return context
+    def form_valid(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super(QuestionCreateView, self).form_valid(request, *args, **kwargs)
+
+class QuestionUpdateView(UpdateView):
+    model = Question
+    template_name = 'polls/question_form.html'
+    fields = ('question_text', 'pub_date', )
+    success_url = reverse_lazy('polls_all')
+    success_message = 'Pergunta atualizada com sucesso.'
+    def get_context_data(self, **kwargs):
+        context = super(QuestionUpdateView, self).get_context_data(**kwargs)
+        context['form_title'] = 'Editando a pergunta'
+        return context
+    def form_valid(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super(QuestionUpdateView, self).form_valid(request, *args, **kwargs)
+
+class QuestionDeleteView(LoginRequiredMixin, DeleteView):
+    model = Question
+    template_name = 'polls/question_confirm_delete.html'
+    success_url = reverse_lazy('polls_all')
+    success_message = 'Pergunta excluída com sucesso.'
+    def form_valid(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super(QuestionDeleteView, self).form_valid(request, *args, **kwargs)
+
+class QuestionDetailView(DetailView):
+    model = Question
+    template_name = 'polls/question_detail.html'
+    context_object_name = 'question'
+
+class QuestionListView(ListView):
+    model = Question
+    template_name = 'polls/question_list.html'
+    context_object_name = 'questions'
+
+class SobreTemplateView(TemplateView):
+    template_name = 'polls/sobre.html'
+
+class QuestionListView(ListView):
+    model = Question
+    template_name = 'polls/question_list.html'
+    context_object_name = 'questions'
+    paginate_by = 2 # quantidade de itens por página
+    ordering = ['-pub_date'] # ordenar pela data de publicação de forma inversão
+
+    
+     
