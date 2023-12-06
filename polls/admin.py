e85634557561c24084a8b01cdff93815f8c5749b
arthur <arthurxavier1408@gmail.com>
2023-12-06 08:13:54 -0300

    atualização geralzinha

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from polls.models import Question, Choice
+from .models import Question
+from .models import Choice
 
 admin.site.register(Question)
 admin.site.register(Choice)
